7edb70896f3a126e8489caa66d5d4484e41ad096
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-09-26 11:22:51 -0700

    sample truffle-config.js

---
+++ truffle-config.js
@@ -0,0 +1,38 @@
+const { mnemonic, secret, password: passphrase, email } = require("./faucet.json");
+
+module.exports = {
+  // see <http://truffleframework.com/docs/advanced/configuration>
+  // for more details on how to specify configuration options!
+  networks: {
+    development: {
+      host: "https://rpcalpha.tzbeta.net",
+      port: 80,
+      network_id: "*",
+      secret,
+      mnemonic,
+      passphrase,
+      email,
+      type: "tezos"
+    },
+    alphanet: {
+      host: "https://rpcalpha.tzbeta.net",
+      port: 80,
+      network_id: "*",
+      secret,
+      mnemonic,
+      passphrase,
+      email,
+      type: "tezos"
+    },
+    zeronet: {
+      host: "https://rpczero.tzbeta.net",
+      port: 80,
+      network_id: "*",
+      secret,
+      mnemonic,
+      passphrase,
+      email,
+      type: "tezos"
+    }
+  }
+};
