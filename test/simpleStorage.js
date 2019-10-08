5fac97a39d2fda6a3e17bf86a49eac2c88d81774
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-10-08 15:47:28 -0700

    add simpleStorage sample test

---
+++ test/simpleStorage.js
@@ -0,0 +1,10 @@
+const SimpleStorage = artifacts.require("SimpleStorage");
+
+contract('SimpleStorage', () => {
+  it("...should store the integer 89.", async() => {
+    const simpleStorageInstance = await SimpleStorage.deployed()
+    await simpleStorageInstance.main(89);
+    const storedInt = await simpleStorageInstance.storage();
+    assert.equal(storedInt, 89, "The integer 89 was not stored.");
+    });
+});
