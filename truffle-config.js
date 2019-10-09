b928ad2fdc5fcc8bb04a19ef81b237cd1c3cc1dc
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-10-09 09:14:10 -0700

    add babylonnet network config (truffle-config.js)

--- truffle-config.js
+++ truffle-config.js
@@ -24,6 +24,16 @@ module.exports = {
       email,
       type: "tezos"
     },
+    babylonnet: {
+      host: "https://babylonnet-node.tzscan.io",
+      port: 80,
+      network_id: "*",
+      secret,
+      mnemonic,
+      passphrase,
+      email,
+      type: "tezos"
+    },
     zeronet: {
       host: "https://rpczero.tzbeta.net",
       port: 80,
