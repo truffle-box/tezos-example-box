36e51b5971ae412a1a083cab1788c67a4d992317
Cruz Molina <cruz.adam.molina@gmail.com>
2020-04-03 14:13:00 -0700

    Fix docker sandbox issue

--- scripts/sandbox/start_sandbox.sh
+++ scripts/sandbox/start_sandbox.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Starting sandbox..."
-docker run --rm --name flextesa-sandbox --detach -p 8732:20000 stovelabs/image-babylonbox-run-archive:latest babylonbox-archive start
+docker run --rm --name flextesa-sandbox --detach -p 8732:20000 stovelabs/image-babylonbox-run-archive:latest sandbox-archive start
