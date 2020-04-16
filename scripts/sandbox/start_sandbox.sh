50fc4867870cd79cfc09036aa52362baf7ba5a68
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2020-04-16 07:00:39 -0700

    update sandbox script to use carthage protocol

--- scripts/sandbox/start_sandbox.sh
+++ scripts/sandbox/start_sandbox.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Starting sandbox..."
-docker run --rm --name flextesa-sandbox --detach -p 8732:20000 stovelabs/image-babylonbox-run-archive:latest sandbox-archive start
+docker run --rm --name flextesa-sandbox --detach -p 8732:20000 stovelabs/image-carthagebox-run-archive:latest sandbox-archive start
